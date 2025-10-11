a3090aca5f293cdd367644686dd1ca5aeb185376
ducanh <anhductranminh@gmail.com>
2025-10-11 23:56:38 +0700

    checkfall

--- Assets/Scrips/Tile.cs
+++ Assets/Scrips/Tile.cs
@@ -1,26 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Callbacks;
 using UnityEngine;
+using DG.Tweening;
 
 public class Tile : MonoBehaviour
 {
-    [SerializeField] private float tileSpeed = 2;
+    [SerializeField] private float tileSpeed = 2f;
+    [SerializeField] private float moveDistance = 50f;
 
-    [SerializeField] private Rigidbody rb;
-    void OnValidate()
+    private void OnEnable()
     {
-        rb = GetComponent<Rigidbody>();
+        transform.DOMoveZ(transform.position.z - moveDistance, moveDistance / tileSpeed)
+            .SetEase(Ease.Linear)
+            .OnComplete(() =>
+            {
+                PoolManager.Instance.ReturnObject(gameObject);
+            });
     }
-    void Update()
+
+    private void OnDisable()
     {
-        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -tileSpeed);
-    }
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("EndPool"))
-        {
-            PoolManager.Instance.ReturnObject(this.gameObject);
-        }
+        transform.DOKill();
     }
 }
